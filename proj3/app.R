31d32a834341859db357629853868a02303cc315
Matthew Ghuneim <mghune2@uic.edu>
2021-04-24 20:24:20 -0500

    done i think...

--- proj3/app.R
+++ proj3/app.R
@@ -18,6 +18,7 @@ library(tidycensus)
 library(tidyverse)
 library(sf)
 library(leaflet)
+library(DT)
 
 
 
@@ -32,17 +33,17 @@ neighborhood <- unique(data$COMMUNITY.AREA.NAME)
 neighborhood2 <- unique(data$COMMUNITY.AREA.NAME)
 
 # selections for the dropdowns
-sources <- list('Electricity' = 'TOTAL.KWH', 'Gas' = 'TOTAL.THERMS', 'Age' = 'AVERAGE.BUILDING.AGE', 
-                'Height' = 'AVERAGE.BUILDING.HEIGHT', 'Population' = 'TOTAL.POPULATION')
+sources <- list('Electricity' = 'TOTAL.KWH', 'Gas' = 'TOTAL.THERMS', 'Age' = 'AVERAGE.BUILDING.AGE', 'Type' = 'BUILDING.TYPE',
+                'Height' = 'AVERAGE.STORIES', 'Population' = 'TOTAL.POPULATION')
 
-months <- list('All' = 'TOTAL', 'January' = 'JANUARY.2010', 'February' = 'FEBRUARY.2010', 'March' = 'MARCH.2010',
+months <- list('January' = 'JANUARY.2010', 'February' = 'FEBRUARY.2010', 'March' = 'MARCH.2010',
                'April' = 'APRIL.2010', 'May' = 'MAY.2010', 'June' = 'JUNE.2010', 'July' = 'JULY.2010',
                'August' = 'AUGUST.2010', 'September' = 'SEPTEMBER.2010', 'October' = 'OCTOBER.2010', 'November' = 'NOVEMBER.2010',
                'December' = 'DECEMBER.2010')
 
-types <- list('All' = 'All', 'Residential' = 'Residential', 'Commmercial' = 'Commercial', 'Industrial' = 'Industrial')
 
-sources2 <- list('Electricity' = 'TOTAL.KWH', 'Gas' = 'TOTAL.THERMS', 'Age' = 'AVERAGE.BUILDING.AGE', 
+
+sources2 <- list('Electricity' = 'TOTAL.KWH', 'Gas' = 'TOTAL.THERMS', 'Age' = 'AVERAGE.BUILDING.AGE', 'Type' = 'BUILDING.TYPE',
                 'Height' = 'AVERAGE.BUILDING.HEIGHT', 'Population' = 'TOTAL.POPULATION')
 
 months2 <- list('January' = 'JANUARY.2010', 'February' = 'FEBRUARY.2010', 'March' = 'MARCH.2010',
@@ -50,8 +51,10 @@ months2 <- list('January' = 'JANUARY.2010', 'February' = 'FEBRUARY.2010', 'March
                'August' = 'AUGUST.2010', 'September' = 'SEPTEMBER.2010', 'October' = 'OCTOBER.2010', 'November' = 'NOVEMBER.2010',
                'December' = 'DECEMBER.2010')
 
-types2 <- list('All' = 'All', 'Residential' = 'Residential', 'Commmercial' = 'Commercial', 'Industrial' = 'Industrial')
 
+cityOptions <- list('Oldest Buildings', 'Newest Buildings', 'Tallest Buildings', 'Most Electricity', 'Most Gas', 'Most Population', 'Most Occupied', 'Renters (Highest %)')
+
+chartMonths <- month.name
 
 
 # shiny application 
@@ -82,8 +85,7 @@ ui <- dashboardPage(
                                    leafletOutput("map1")
                                ),
                                selectInput("Source", "Select a source to visualize", sources, selected = 'Electricity'),
-                               selectInput("Month", "Select a month to visualize", months, selected = 'All'),
-                               selectInput("Type", "Select a type to visualize", types, selected = 'All')
+                               selectInput("Month", "Select a month to visualize", months, selected = 'All')
                         ),
                         column(6,
                                box(title = "Line Graphs of Near West Side", solidHeader = TRUE,
@@ -100,23 +102,21 @@ ui <- dashboardPage(
             # different neighborhood comparisons and different graphs
             tabItem(tabName = "states",
                     fluidRow(
-                        column(5,
+                        column(6,
                                box(title = "Map 1", solidHeader = TRUE,
                                    status = "primary", width = 12,
                                    leafletOutput("map2")
                                ),
                                selectInput("Source2", "Select a source to visualize", sources, selected = 'Electricity'),
-                               selectInput("Month2", "Select a month to visualize", months, selected = 'All'),
-                               selectInput("Type2", "Select a type to visualize", types, selected = 'All')
+                               selectInput("Month2", "Select a month to visualize", months, selected = 'All')
                         ),
-                        column(5,
+                        column(6,
                                box(title = "Map 2", solidHeader = TRUE,
                                    status = "primary", width = 12,
                                    leafletOutput("map3")
                                ),
                                selectInput("Source3", "Select a source to visualize", sources2, selected = 'Electricity'),
-                               selectInput("Month3", "Select a month to visualize", months2, selected = 'All'),
-                               selectInput("Type3", "Select a type to visualize", types2, selected = 'All')
+                               selectInput("Month3", "Select a month to visualize", months2, selected = 'All')
                         ),
                     )
             ),
@@ -144,18 +144,17 @@ ui <- dashboardPage(
                         )
                     )
             ),
-            # Chicago map
             tabItem(tabName = "chi",
-                    column(10,
-                           box(title = "Chicago Map", solidHeader = TRUE,
-                               status = "primary", width = 12,
-                               leafletOutput("map4")
-                           ),
-                           selectInput("Source4", "Select a source to visualize", sources, selected = 'Electricity'),
-                           selectInput("Month4", "Select a month to visualize", months, selected = 'All'),
-                           selectInput("Type4", "Select a type to visualize", types, selected = 'All')
-                    ),
-                    column(1,
+                    fluidRow(
+                        column(8,
+                               box(title = "Mapview Map of Chicago", solidHeader = TRUE,
+                                   status = "primary", width = 12,
+                                   leafletOutput("map4")
+                               ),
+                        ),
+                        column(4, 
+                        selectInput("Source4", "Select a tract to visualize", cityOptions, selected = 'Oldest Buildings')
+                        )
                     )
             ),
             #about
@@ -180,18 +179,33 @@ server <- function(input, output) {
         
         if ('TOTAL.KWH' %in% input$Source | 'TOTAL.THERMS' %in% input$Source){
             if ('All' %in% input$Month & 'TOTAL.KWH' %in% input$Source){
-                p <- paste0(input$Month, ".KWH")
+                m1 <- mapview(chiblocks, zcol = 'TOTAL.KWH')
             }
             else if ((!'All' %in% input$Month) & 'TOTAL.KWH' %in% input$Source){
                 p <- paste0("KWH.", input$Month)
+                m1 <- mapview(chiblocks, zcol = p)
             }
             else if ((!'All' %in% input$Month) & 'TOTAL.THERMS' %in% input$Source){
                 p <- paste0("THERM.", input$Month)
+                m1 <- mapview(chiblocks, zcol = p)
             }
             
         }
+        else if (input$Source == 'AVERAGE.BUILDING.AGE'){
+            m1 <- mapview(chiblocks, zcol = 'AVERAGE.BUILDING.AGE')
+        }
+        else if (input$Source == 'BUILDING.TYPE'){
+            m1 <- mapview(chiblocks, zcol = 'BUILDING.TYPE')
+        }
+        else if (input$Source == 'AVERAGE.STORIES'){
+            m1 <- mapview(chiblocks, zcol = 'AVERAGE.STORIES')
+        }
+        else if (input$Source == 'TOTAL.POPULATION'){
+            m1 <- mapview(chiblocks, zcol = 'TOTAL.POPULATION')
+        }
         
-        mapview(chiblocks, zcol = p)@map
+        
+        m1@map
         
     })
     
@@ -203,13 +217,25 @@ server <- function(input, output) {
         if ('TOTAL.KWH' %in% input$Source2 | 'TOTAL.THERMS' %in% input$Source2){
             if ((!'All' %in% input$Month2) & 'TOTAL.KWH' %in% input$Source2){
                 p <- paste0("KWH.", input$Month2)
+                m1 <- mapview(chiblocks, zcol = p)
             }
             else if ((!'All' %in% input$Month2) & 'TOTAL.THERMS' %in% input$Source2){
                 p <- paste0("THERM.", input$Month2)
+                m1 <- mapview(chiblocks, zcol = p)
             }
         }
+        else if (input$Source2 == 'AVERAGE.BUILDING.AGE'){
+            m1 <- mapview(chiblocks, zcol = 'AVERAGE.BUILDING.AGE')
+        }
+        else if (input$Source2 == 'AVERAGE.STORIES'){
+            m1 <- mapview(chiblocks, zcol = 'AVERAGE.STORIES')
+        }
+        else if (input$Source2 == 'TOTAL.POPULATION'){
+            m1 <- mapview(chiblocks, zcol = 'TOTAL.POPULATION')
+        }
+       
         
-        mapview(chiblocks, zcol = p)@map
+        m1@map
         
     })
     
@@ -221,39 +247,173 @@ server <- function(input, output) {
         if ('TOTAL.KWH' %in% input$Source3 | 'TOTAL.THERMS' %in% input$Source3){
             if ((!'All' %in% input$Month3) & 'TOTAL.KWH' %in% input$Source3){
                 p <- paste0("KWH.", input$Month3)
+                m1 <- mapview(chiblocks, zcol = p)
             }
             else if ((!'All' %in% input$Month3) & 'TOTAL.THERMS' %in% input$Source3){
                 p <- paste0("THERM.", input$Month3)
+                m1 <- mapview(chiblocks, zcol = p)
             }
         }
-        
-        mapview(chiblocks, zcol = p)@map
+        else if (input$Source3 == 'AVERAGE.BUILDING.AGE'){
+            m1 <- mapview(chiblocks, zcol = 'AVERAGE.BUILDING.AGE')
+        }
+        else if (input$Source3 == 'AVERAGE.STORIES'){
+            m1 <- mapview(chiblocks, zcol = 'AVERAGE.STORIES')
+        }
+        else if (input$Source3 == 'TOTAL.POPULATION'){
+            m1 <- mapview(chiblocks, zcol = 'TOTAL.POPULATION')
+        }
+        m1@map
         
     })
     
     output$map4 <- renderLeaflet({
-        chiblocks <- subset(chiblocks, GEOID10 %in% data$GEOID10)
-        chiblocks <- merge(chiblocks, data, by.x = 'GEOID10', by.y = 'GEOID10')
+        chidata <- data
+        chiblocks <- subset(chiblocks, GEOID10 %in% chidata$GEOID10)
         
-        p <- input$Source
-        if ('TOTAL.KWH' %in% input$Source4 | 'TOTAL.THERMS' %in% input$Source4){
-            if ((!'All' %in% input$Month4) & 'TOTAL.KWH' %in% input$Source4){
-                p <- paste0("KWH.", input$Month4)
-            }
-            else if ((!'All' %in% input$Month4) & 'TOTAL.THERMS' %in% input$Source4){
-                p <- paste0("THERM.", input$Month4)
-            }
+        if (input$Source4 == 'Oldest Buildings'){
+            chiblocks <- merge(chiblocks, chidata, by.x = 'GEOID10', by.y = 'GEOID10')
+            chiblocks <- head(chiblocks[order(chiblocks$AVERAGE.BUILDING.AGE, decreasing = TRUE),], n = 180)
+            m1 <- mapview(chiblocks, zcol = 'AVERAGE.BUILDING.AGE')
+        }
+        else if (input$Source4 == 'Newest Buildings'){
+            chiblocks <- merge(chiblocks, chidata, by.x = 'GEOID10', by.y = 'GEOID10')
+            chiblocks <- head(chiblocks[order(chiblocks$AVERAGE.BUILDING.AGE, decreasing = FALSE),], n = 180)
+            m1 <- mapview(chiblocks, zcol = 'AVERAGE.BUILDING.AGE')
+        }
+        else if (input$Source4 == 'Tallest Buildings'){
+            chiblocks <- merge(chiblocks, chidata, by.x = 'GEOID10', by.y = 'GEOID10')
+            chiblocks <- head(chiblocks[order(chiblocks$AVERAGE.STORIES, decreasing = TRUE),], n = 180)
+            m1 <- mapview(chiblocks, zcol = 'AVERAGE.STORIES')
+        }
+        else if (input$Source4 == 'Most Electricity'){
+            chiblocks <- merge(chiblocks, chidata, by.x = 'GEOID10', by.y = 'GEOID10')
+            chiblocks <- head(chiblocks[order(chiblocks$TOTAL.KWH, decreasing = TRUE),], n = 180)
+            m1 <- mapview(chiblocks, zcol = 'TOTAL.KWH')
+        }
+        else if (input$Source4 == 'Most Gas'){
+            chiblocks <- merge(chiblocks, chidata, by.x = 'GEOID10', by.y = 'GEOID10')
+            chiblocks <- head(chiblocks[order(chiblocks$TOTAL.THERMS, decreasing = TRUE),], n = 180)
+            m1 <- mapview(chiblocks, zcol = 'TOTAL.THERMS')
+        }
+        else if (input$Source4 == 'Most Population'){
+            chiblocks <- merge(chiblocks, chidata, by.x = 'GEOID10', by.y = 'GEOID10')
+            chiblocks <- head(chiblocks[order(chiblocks$TOTAL.POPULATION, decreasing = TRUE),], n = 180)
+            m1 <- mapview(chiblocks, zcol = 'TOTAL.POPULATION')
+        }
+        else if (input$Source4 == 'Most Occupied'){
+            chiblocks <- merge(chiblocks, chidata, by.x = 'GEOID10', by.y = 'GEOID10')
+            chiblocks <- head(chiblocks[order(chiblocks$OCCUPIED.UNITS.PERCENTAGE, decreasing = TRUE),], n = 180)
+            m1 <- mapview(chiblocks, zcol = 'OCCUPIED.UNITS.PERCENTAGE')
+        }
+        else if (input$Source4 == 'Renters (Highest %)'){
+            chiblocks <- merge(chiblocks, chidata, by.x = 'GEOID10', by.y = 'GEOID10')
+            chiblocks <- head(chiblocks[order(chiblocks$RENTER.OCCUPIED.HOUSING.PERCENTAGE, decreasing = TRUE),], n = 180)
+            m1 <- mapview(chiblocks, zcol = 'RENTER.OCCUPIED.HOUSING.PERCENTAGE')
         }
         
-        mapview(chiblocks, zcol = p)@map
+    
+        m1@map
         
     })
     
     # all functions for the graphs
     output$plot1 <- renderPlot({
+        data2 <- subset(data, data$COMMUNITY.AREA.NAME == "Near West Side")
+        if('TOTAL.KWH' %in% input$Source){
+            kwh <- data.frame(colSums(data2[, 5:16], na.rm = TRUE))
+            colnames(kwh)[1] <- c("Totals")
+            kwh$month <- chartMonths
+            kwh$month = factor(kwh$month, levels = month.name)
+            ggplot(kwh, aes(x = month, y = Totals, fill = month)) + geom_bar(stat = 'identity')  + xlab('Month') + ylab('Total Electricity Usage')
+        }
+        else if ('TOTAL.THERMS' %in% input$Source){
+            therm <- data.frame(colSums(data2[, 20:31], na.rm = TRUE))
+            colnames(therm)[1] <- c("Totals")
+            therm$month <- chartMonths
+            therm$month = factor(therm$month, levels = month.name)
+            ggplot(therm, aes(x = month, y = Totals, fill = month)) + geom_bar(stat = 'identity') + xlab('Month') + ylab('Total Gas Usage')
+        }
+    })
+    output$plot2 <- renderPlot({
+        data2 <- subset(data, data$COMMUNITY.AREA.NAME == input$Community)
+        if('TOTAL.KWH' %in% input$Source2){
+            kwh <- data.frame(colSums(data2[, 5:16], na.rm = TRUE))
+            colnames(kwh)[1] <- c("Totals")
+            kwh$month <- chartMonths
+            kwh$month = factor(kwh$month, levels = month.name)
+            ggplot(kwh, aes(x = month, y = Totals, fill = month)) + geom_bar(stat = 'identity') + xlab('Month') + ylab('Total Electricity Usage')
+        }
+        else if ('TOTAL.THERMS' %in% input$Source2){
+            therm <- data.frame(colSums(data2[, 20:31], na.rm = TRUE))
+            colnames(therm)[1] <- c("Totals")
+            therm$month <- chartMonths
+            therm$month = factor(therm$month, levels = month.name)
+            ggplot(therm, aes(x = month, y = Totals, fill = month)) + geom_bar(stat = 'identity') + xlab('Month') + ylab('Total Gas Usage')
+        }
+    })
+    output$plot3 <- renderPlot({
+        data2 <- subset(data, data$COMMUNITY.AREA.NAME == input$Community2)
+        if('TOTAL.KWH' %in% input$Source3){
+            kwh <- data.frame(colSums(data2[, 5:16], na.rm = TRUE))
+            colnames(kwh)[1] <- c("Totals")
+            kwh$month <- chartMonths
+            kwh$month = factor(kwh$month, levels = month.name)
+            ggplot(kwh, aes(x = month, y = Totals, fill = month)) + geom_bar(stat = 'identity') + xlab('Month') + ylab('Total Electricity Usage')
+        }
+        else if ('TOTAL.THERMS' %in% input$Source3){
+            therm <- data.frame(colSums(data2[, 20:31], na.rm = TRUE))
+            colnames(therm)[1] <- c("Totals")
+            therm$month <- chartMonths
+            therm$month = factor(therm$month, levels = month.name)
+            ggplot(therm, aes(x = month, y = Totals, fill = month)) + geom_bar(stat = 'identity') + xlab('Month') + ylab('Total Gas Usage')
+        }
+    })
+    
+    # all functions for the tables
+    output$tab1 <- DT::renderDataTable({
+        data2 <- subset(data, data$COMMUNITY.AREA.NAME == "Near West Side")
         if ('TOTAL.KWH' %in% input$Source){
-            kwh <- data.frame(colSums(data[, 5:16]), na.rm = TRUE)
-            
+            kwh <- data.frame(colSums(data2[, 5:16], na.rm = TRUE))
+            colnames(kwh)[1] <- c("Totals")
+            kwh$month <- chartMonths
+            datatable(kwh, options = list(pageLength = 2), rownames = FALSE)
+        }
+        else if ('TOTAL.THERMS' %in% input$Source){
+            therm <- data.frame(colSums(data2[, 20:31], na.rm = TRUE))
+            colnames(therm)[1] <- c("Totals")
+            therm$month <- chartMonths
+            datatable(therm, options = list(pageLength = 2), rownames = FALSE)
+        }
+    })
+    output$tab2 <- DT::renderDataTable({
+        data2 <- subset(data, data$COMMUNITY.AREA.NAME == input$Community)
+        if ('TOTAL.KWH' %in% input$Source2){
+            kwh <- data.frame(colSums(data2[, 5:16], na.rm = TRUE))
+            colnames(kwh)[1] <- c("Totals")
+            kwh$month <- chartMonths
+            datatable(kwh, rownames = FALSE)
+        }
+        else if ('TOTAL.THERMS' %in% input$Source2){
+            therm <- data.frame(colSums(data2[, 20:31], na.rm = TRUE))
+            colnames(therm)[1] <- c("Totals")
+            therm$month <- chartMonths
+            datatable(therm, rownames = FALSE)
+        }
+    })
+    output$tab3 <- renderDataTable({
+        data2 <- subset(data, data$COMMUNITY.AREA.NAME == input$Community2)
+        if ('TOTAL.KWH' %in% input$Source3){
+            kwh <- data.frame(colSums(data2[, 5:16], na.rm = TRUE))
+            colnames(kwh)[1] <- c("Totals")
+            kwh$month <- chartMonths
+            datatable(kwh, rownames = FALSE)
+        }
+        else if ('TOTAL.THERMS' %in% input$Source3){
+            therm <- data.frame(colSums(data2[, 20:31], na.rm = TRUE))
+            colnames(therm)[1] <- c("Totals")
+            therm$month <- chartMonths
+            datatable(therm, rownames = FALSE)
         }
     })
 
